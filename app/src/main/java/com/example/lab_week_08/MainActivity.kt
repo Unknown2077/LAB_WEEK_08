6b46dcf283bb9cbf794c371b1f226392d698e035
Muhammad Aryasatya Triputra <m.aryasatya.triputra@gmail.com>
2025-10-30 12:43:07 +0700

    Commit No. 2 – add first foreground service

--- app/src/main/java/com/example/lab_week_08/MainActivity.kt
+++ app/src/main/java/com/example/lab_week_08/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.example.lab_week_08
 
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.work.Constraints
 import androidx.work.Data
 import androidx.work.NetworkType
@@ -29,6 +35,13 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right,
                 systemBars.bottom)
             insets
+        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            if (checkSelfPermission(android.Manifest.permission.POST_NOTIFICATIONS) !=
+                PackageManager.PERMISSION_GRANTED) {
+
+                requestPermissions(arrayOf(android.Manifest.permission.POST_NOTIFICATIONS), 1)
+            }
         }
         //Create a constraint of which your workers are bound to.
         //Here the workers cannot execute the given process if
@@ -85,6 +98,13 @@ class MainActivity : AppCompatActivity() {
                     showResult("Second process is done")
                 }
             }
+        workManager.getWorkInfoByIdLiveData(secondRequest.id)
+            .observe(this) { info ->
+                if (info?.state?.isFinished == true) {
+                    showResult("Second process is done")
+                    launchNotificationService()
+                }
+            }
     }
 
     //Build the data into the correct format before passing it to the worker as
@@ -97,4 +117,27 @@ class MainActivity : AppCompatActivity() {
     private fun showResult(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
+    //Launch the NotificationService
+    private fun launchNotificationService() {
+        //Observe if the service process is done or not
+        //If it is, show a toast with the channel ID in it
+        NotificationService.trackingCompletion.observe(
+            this) { Id ->
+            showResult("Process for Notification Channel ID $Id is done!")
+        }
+
+        //Create an Intent to start the NotificationService
+        //An ID of "001" is also passed as the notification channel ID
+        val serviceIntent = Intent(this,
+            NotificationService::class.java).apply {
+            putExtra(EXTRA_ID, "001")
+        }
+
+        //Start the foreground service through the Service Intent
+        ContextCompat.startForegroundService(this, serviceIntent)
+    }
+    companion object{
+        const val EXTRA_ID = "Id"
+    }
+
 }
